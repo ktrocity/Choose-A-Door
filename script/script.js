9e85fb74692e94819285992e9dcbd2e6c63d827d
Keli Rice <82687494+ktrocity@users.noreply.github.com>
2025-01-30 22:44:55 -0600

    face opposite

--- script/script.js
+++ script/script.js
@@ -12,8 +12,8 @@ const LEVEL1 = [
     1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
     17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32,
     33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48,
-    49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64,
-    65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 
+    49, 11, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64,
+    65, 27, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 
     81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96,
     97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112,
     113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128,];
@@ -79,8 +79,8 @@ window.addEventListener('load', function() {
 
     let debug = false;
 
-// I think this is where we're rendering the tile map AFTER the resources are loaded, but I don't totally rememebr how it all works anymore to be honest.
-
+// Rendering the Tile Map?
+    
 function drawLevel() {
     for (let row = 0; row < ROWS; row++) {
         for (let col = 0; col < COLUMNS; col++) {
@@ -110,7 +110,7 @@ function drawLevel() {
         currentFrame: 0,
         frames: [],
         state: 'idle',
-        direction: 'right',
+        direction: 'left',
         frameTimer: 0,
         frameDelay: 6,
         velocityY: 30,
@@ -123,6 +123,7 @@ const frameWidth = 400;
 const frameHeight = 400;
 const totalRunningFrames = 8;
 const totalIdleFrames = 8;
+
     
 playerSpriteSheet.onload = function() {
     for (let i = 0; i < totalRunningFrames; i++) {
@@ -136,7 +137,7 @@ player.idleFrames = [];
             x: i * frameWidth,
             y: 0 });}
 
-    gameLoop();};
+gameLoop();};
 
 // Rendering the Player Character
 
@@ -173,12 +174,8 @@ function drawPlayer(ctx) {
         player.currentFrame++;
 
         if (player.state === 'idle') {
-            player.currentFrame %= totalIdleFrames; // Loop through idle animation
-        } else {
-            player.currentFrame %= totalRunningFrames; // Loop through running animation
-        }
-    }
-}    
+            player.currentFrame %= totalIdleFrames;}else {
+            player.currentFrame %= totalRunningFrames;}}}    
 
 
 // Left & Right Movement
